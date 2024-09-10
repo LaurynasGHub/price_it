d8c4d4997ef5cce7f514780200d2b38567806ef8
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-09-11 00:20:30 +0300

    FINALLY fixed the app background issue. other updates
    changes to main.jsx- refactored a bit, removed some cluster code. Background issue fixed by adding min-height to app.scss .App class

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -25,28 +25,21 @@ function Main() {
   }
 
   return (
-    <div className="default-div rounded py-2 m-3 default-text">
-      <div className="container-fluid pb-2">
-        <div className="row">
-          <div className="col-8 col-sm-10 col-md-10">
-            <SearchBar handleInputChange={handleInputChange} />
-          </div>
-          <div className="col-4 col-sm-2 col-md-2">
-            <SearchButton
-              onClickFunction={searchForResults}
-              btnTitle={'Search'}
-            />
-          </div>
+    <div className="container-fluid pb-2">
+      <div className="row">
+        <div className="col-8 col-sm-10 col-md-10">
+          <SearchBar handleInputChange={handleInputChange} />
+        </div>
+        <div className="col-4 col-sm-2 col-md-2">
+          <SearchButton onClickFunction={searchForResults} />
         </div>
       </div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-8">
-            <ResultsWindow />
-          </div>
-          <div className="col-md-4">
-            <MostSearchedItems searchResults={searchResults} />
-          </div>
+      <div className="row mt-2">
+        <div className="col-md-8">
+          <ResultsWindow />
+        </div>
+        <div className="col-md-4">
+          <MostSearchedItems searchResults={searchResults} />
         </div>
       </div>
     </div>
--- src/components/SearchButton/SearchButton.jsx
+++ src/components/SearchButton/SearchButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function SearchButton({ onClickFunction, btnTitle }) {
+function SearchButton({ onClickFunction }) {
   return (
     <button
       className="p-2 w-100 non-styled-item underline-button default-text"
       onClick={onClickFunction}
     >
-      {btnTitle}
+      Search
     </button>
   );
 }
