b2d1bbcce8483bd4f9d9a7ccff6d017853bba4da
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-09-10 14:07:48 +0300

    moved button to a separate component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,17 +8,17 @@ import MostSearchedItems from '../MostSearchedItems/MostSearchedItems';
 function SearchCard() {
   return (
     <div className="default-div custom-border rounded py-2 m-1 default-text">
-      <div class="container-fluid">
-        <div class="row">
+      <div className="container-fluid">
+        <div className="row">
           <div className="col-md-12 ">
             <SearchBar />
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-8">
+        <div className="row">
+          <div className="col-md-8">
             <ResultCards />
           </div>
-          <div class="col-md-4">
+          <div className="col-md-4">
             <MostSearchedItems />
           </div>
         </div>
