4e5fbd22a22ecbb83ac1300c0c4a90d2992b8e67
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-09-18 21:23:00 +0300

    added useEffect to MostSearchedItems.jsx- changes classNames dependant on page size

--- src/components/MostSearchedItems/MostSearchedItems.jsx
+++ src/components/MostSearchedItems/MostSearchedItems.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function MostSearchedItems() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    //if window size is below 600px gives true
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 600);
+    };
+
+    // Add event listener to window resize
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   function getMostSearchedData() {
     // Function should get the most searched items from backend
     //
@@ -14,7 +32,13 @@ function MostSearchedItems() {
     //
   }
   return (
-    <div className="default-div custom-border rounded p-3 my-3 default-text">
+    <div
+      className={
+        isMobile
+          ? 'default-div custom-border rounded p-3 my-1 default-text'
+          : 'default-div custom-border rounded p-3 my-3 default-text'
+      }
+    >
       <h5>Most popular searches:</h5>
       <li>1. Duona</li>
       <li>2. Pienas</li>
