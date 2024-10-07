ba4c424f79d04603517d6b3cf4ff045a63709ddf
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-10-07 15:41:00 +0300

    Fixed shop prices maping, added loader for when fetching data

--- src/components/CostOfMainItemsCart/CostOfMainItemsCart.jsx
+++ src/components/CostOfMainItemsCart/CostOfMainItemsCart.jsx
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../../context/AppContext';
 
 function MostSearchedItems() {
   const { mainCartData, setMainCartData } = useContext(AppContext);
   const { mainCartPrices, setMainCartPrices } = useContext(AppContext);
 
-  function showData() {
-    console.log(mainCartPrices);
-  }
   return (
     <div className="default-div custom-border rounded p-3 my-3 default-text">
       <h5>Cost of main products cart:</h5>
@@ -15,9 +12,18 @@ function MostSearchedItems() {
         Main products cart consists of the main everyday items: milk, bread,
         butter...
       </p>
-      {mainCartPrices.map((item) => (
-        <li key={item.shop}>{item.shop}</li>
-      ))}
+      {mainCartPrices.length < 1 ? (
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          <div className="loader">...</div>
+        </div>
+      ) : (
+        mainCartPrices.map((item) => (
+          <li key={item.shop}>
+            {item.shop.charAt(0).toUpperCase() + item.shop.slice(1)},{' '}
+            {item.price} €.
+          </li>
+        ))
+      )}
     </div>
   );
 }
