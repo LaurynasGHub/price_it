9dac6afbdae077a4e6512fd778bda5957b6f5a03
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-10-09 21:53:56 +0300

    Added profile screen

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import './profile.scss';
 
 function Profile() {
-  return <div>Profile</div>;
+  const getRegisterUsername = useRef();
+  const getRegisterPassword = useRef();
+
+  return (
+    <div className="default-div default-text p-4 text-center profile-main-div">
+      <form className="profile-form">
+        <input
+          type="text"
+          id="userName"
+          placeholder="user name"
+          className="profile-input p-2 default-text"
+          ref={getRegisterUsername}
+        ></input>
+
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          autoComplete="on"
+          className="profile-input p-2 default-text"
+          ref={getRegisterPassword}
+        ></input>
+      </form>
+      <div className="m-3">
+        <button className="underline-button default-text non-styled-item">
+          Log in
+        </button>
+      </div>
+      <div className="register-div">
+        <p className="mb-0">Not a member yet?</p>
+        <button className="underline-button default-text non-styled-item">
+          Register
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
