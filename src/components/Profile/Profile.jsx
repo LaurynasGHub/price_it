ce6949bf9d43211fa7982e108745f68dc606b918
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-10-10 23:53:42 +0300

    Added user login

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,42 +1,92 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+
+import { cfg } from '../../cfg/cfg';
 
 import './profile.scss';
 
 function Profile() {
-  const getRegisterUsername = useRef();
-  const getRegisterPassword = useRef();
+  const getPassword = useRef();
+  const getUsername = useRef();
+  const [loading, setLoading] = useState();
+  const [logInError, setLogInError] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogIn = async (e) => {
+    e.preventDefault();
+
+    const username = getUsername.current.value;
+    const password = getPassword.current.value;
+
+    try {
+      setLoading(true);
+
+      const response = await fetch(`${cfg.API.HOST}/user/login`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'Application/json',
+
+          Authorization: 'Bearer token',
+        },
+
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        setLogInError('username or password is incorrect');
+        throw new Error('username or password is incorrect');
+      } else {
+        setLogInError('');
+        const user = await response.json();
+        console.log('successfully logged in', user);
+        setLoggedIn(true);
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div className="default-div default-text p-4 text-center profile-main-div">
-      <form className="profile-form">
-        <input
-          type="text"
-          id="userName"
-          placeholder="user name"
-          className="profile-input p-2 default-text"
-          ref={getRegisterUsername}
-        ></input>
-
-        <input
-          type="password"
-          id="password"
-          placeholder="password"
-          autoComplete="on"
-          className="profile-input p-2 default-text"
-          ref={getRegisterPassword}
-        ></input>
-      </form>
-      <div className="m-3">
-        <button className="underline-button default-text non-styled-item">
-          Log in
-        </button>
-      </div>
-      <div className="register-div">
-        <p className="mb-0">Not a member yet?</p>
-        <button className="underline-button default-text non-styled-item">
-          Register
-        </button>
-      </div>
+    <div>
+      {!loggedIn ? (
+        <div className="default-div default-text p-4 text-center profile-main-div">
+          <form className="profile-form">
+            <input
+              type="text"
+              id="userName"
+              placeholder="user name"
+              className="profile-input p-2 default-text"
+              ref={getUsername}
+            ></input>
+            <input
+              type="password"
+              id="password"
+              placeholder="password"
+              autoComplete="on"
+              className="profile-input p-2 default-text"
+              ref={getPassword}
+            ></input>
+          </form>
+          <div className="error-message mt-3">{logInError}</div>
+          <div className="m-3">
+            <button
+              className="underline-button default-text non-styled-item"
+              onClick={handleLogIn}
+            >
+              Log in
+            </button>
+          </div>
+          <div className="register-div">
+            <p className="mb-0">Not a member yet?</p>
+            <button className="underline-button default-text non-styled-item">
+              Register
+            </button>
+          </div>
+        </div>
+      ) : (
+        <h2>Welcome</h2>
+      )}
     </div>
   );
 }
