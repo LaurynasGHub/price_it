9395a350b401184cd868bc026c42e1d4b5098a15
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2025-12-07 22:32:39 +0200

    Updates to json and fixed maping in ShopsSelection.jsx

--- src/components/ShopsSelection/ShopsSelection.jsx
+++ src/components/ShopsSelection/ShopsSelection.jsx
@@ -7,12 +7,31 @@ import shops from '../../utils/shopList.json';
 function ShopsSelection() {
   return (
     <div
-      className="default-div default-text p-2 
-    custom-border rounded mt-2 d-flex flex-row flex-wrap gap-2"
+      className="default-div default-text p-2 my-2
+    custom-border rounded d-flex flex-column flex-wrap"
     >
-      {shops.map((shop) => (
-        <ShopSelectButton shopName={shop.name} />
-      ))}
+      <div>
+        <p className="mb-0 m-1">Groceries</p>
+        <div
+          className="default-div default-text
+         d-flex flex-row flex-wrap gap-2"
+        >
+          {shops.shops.groceries.map((shop) => (
+            <ShopSelectButton shopName={shop.name} />
+          ))}
+        </div>
+      </div>
+      <div>
+        <p className="mb-0 m-1">Alcohol</p>
+        <div
+          className="default-div default-text
+         d-flex flex-row flex-wrap gap-2"
+        >
+          {shops.shops.alcohol.map((shop) => (
+            <ShopSelectButton shopName={shop.name} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
