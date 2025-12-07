cf3726d83bb4a2d2fd4ab42e96f2eabdab181f95
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2025-12-07 22:18:05 +0200

    Edits to ShopsSelection, added json with shops

--- src/components/ShopsSelection/ShopsSelection.jsx
+++ src/components/ShopsSelection/ShopsSelection.jsx
@@ -1,10 +1,18 @@
 // components
 import ShopSelectButton from '../ShopsSelection/ShopSelectButton/ShopSelectButton';
 
+// shop list
+import shops from '../../utils/shopList.json';
+
 function ShopsSelection() {
   return (
-    <div className="default-div default-text p-2 custom-border rounded mt-2">
-      <ShopSelectButton shopName="Maxima" />
+    <div
+      className="default-div default-text p-2 
+    custom-border rounded mt-2 d-flex flex-row flex-wrap gap-2"
+    >
+      {shops.map((shop) => (
+        <ShopSelectButton shopName={shop.name} />
+      ))}
     </div>
   );
 }
